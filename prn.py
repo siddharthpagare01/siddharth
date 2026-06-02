cd6d143af9d1e823a36ae0058d1d814a6d1b1f7e
siddharthpagare01 <143196285+siddharthpagare01@users.noreply.github.com>
2026-06-02 14:23:36 +0530

    Update prn.py

--- prn.py
+++ prn.py
@@ -1,269 +1,34 @@
-ACTS, PUNE
-PG-DITISS 1
-Suggested Teaching Guideline For
-Concept of Operating Systems & Administration PG-DITISS August 2024
-Duration: 90 Classroom hrs + 90 Lab hrs (Total: 180 Hrs)
-Objective: To introduce the student to concepts of Operating Systems & Administration.
-Evaluation method: Theory exam – 40% weightage
- Lab exam – 40% weightage 
- Internal Assessment – 20% weightage
-List of Books / Other training material
-Courseware: The Complete Reference Linux by Richard Petersen
-Reference book:
-• Mastering Windows Server 2016 R2 by Brian Svidergol, Vladimir Meloski
-• Windows Server 2022 Administration Fundamentals by Bekim Dauti
-Note: Encourage candidates to implement lab using scripts.
-Windows Operating System and Security Issue (80 Hrs - 40 Hrs Theory + 40 Hrs Lab)
-Session 1 & 2:
- Overview of windows operating system
- Installation of windows operating system
-Lab:
- Install, upgrade, and migrate servers and workloads
- Create, manage, and maintain images for deployment
-Session 3:
- Concept of Windows Server Backup (WSB)
-Lab:
- Implement Windows Server Backup (WSB)
+(MySQL): 10 Theory +10 Lab
+Session 1:
+ Concepts of databases, tables, and schemas.
+ Syntax and examples for creating databases.
+ Best practices for organizing and naming databases.
+ Syntax and examples for dropping databases.
+Lab Assignment:
+ Create a MYSQL user.
+ Create a database add tables and then add data into those tables.
+ Delete/Modify a Table or Database.
+Session 2 & 3:
+ Writing SELECT statements to fetch data.
+ Using WHERE clause for filtering data.
+ Sorting data using ORDER BY.
+ Basic JOIN operations to combine data from multiple tables.
+Lab Assignment:
+ Write SELECT statements to retrieve specific data from a database.
+ Use WHERE clauses to filter data and extract relevant information.
+ Apply ORDER BY clauses to sort retrieved data for analytical purposes.
 Session 4 & 5:
- Implementation, planning and maintaining of active directory 
-infrastructure
-Lab:
- Implementing and administering Active Directory
- User accounts and groups in an Active Directory Domain
- Implementing an Active Directory Domain Services (ADDS)
- Domain Controller (DC)
- Additional Domain Controller (ADC)
- Client (Windows 10/11)
- Member Server (MS)
+ Using aggregate functions (SUM, COUNT, AVG).
+ GROUP BY and HAVING clauses for grouped data.
+ Subqueries and nested queries.
 ACTS, PUNE
 PG-DITISS 2
-Suggested Teaching Guideline For
-Concept of Operating Systems & Administration PG-DITISS August 2024
-Session 6 & 7:
- Configuring DNS
- Implement DHCP and IPAM
-Lab:
- Configuration of DNS server
- Install and configure DHCP
- Manage and maintain DHCP
- Implement and Maintain IP Address Management (IPAM)
-Session 8:
- Local policies and Group Policies
-Lab:
- Implement Local policies & Group policies
-Session 9:
- Implement Network Connectivity and Remote Access Solutions
-Lab:
- Implement network connectivity solutions
- Implement virtual private network (VPN) and Direct Access solutions
-Session 10:
- Concept of IIS Web Server
-Lab:
- Implement of IIS Web Server
-Session 11:
- Concept of Core and Distributed Network Solutions
-Lab:
- Implement IPv4 and IPv6 addressing
- Implement Distributed File System (DFS) and Branch Office solutions
-Session 12:
- Concept of Windows Deployment Service (WDS)
-ACTS, PUNE
-PG-DITISS 3
-Suggested Teaching Guideline For
-Concept of Operating Systems & Administration PG-DITISS August 2024
-Lab:
- Deploying Windows 10/11 using WDS
-Session 13 & 14: Hyper-V & Storage Solutions
- Implement Hyper-V
- Configure disks and volumes
- Implement server storage
- Implement data deduplication
-Lab:
- Install and configure Hyper-V
- Configure virtual machine (VM) setting
-Session 15:
- Concept of File Server Resource Manager (FSRM)
-Lab:
- Implement File Server Resource Manager (FSRM)
-Session 16:
- Concept of Network Policy Server (NPS)
-Lab:
- Implement Network Policy Server (NPS)
-Session 17:
- Concept of Network Load Balancing (NLB)
- Troubleshooting
-Lab:
- Implement of Network Load Balancing (NLB)
- Maintenance and troubleshooting
- Introduction to Microsoft Windows 10 & 11 security
- Security issues at the Active Directory level
- Evaluating and analyzing workstation security
- Securing Windows services
-Session 18:
- Concept of Exchange server
-Lab:
- Installing and implementing of Exchange Server.
-ACTS, PUNE
-PG-DITISS 4
-Suggested Teaching Guideline For
-Concept of Operating Systems & Administration PG-DITISS August 2024
-Session 19 & 20:
- Power Shell
-Lab:
- Windows Power Shell Technology Background and Overview
- Power shell Error Handling and Debugging
- Windows Administration using power shell
- Background Jobs and Remote Administration
- Windows Power Shell Tips & Re-Using Scripts and Functions
-Linux Operating System and Security (100Hrs) (50 Hrs Theory + 50 hrs Lab)
-Session 1 & 2:
- Introduction to Linux
- The Linux File System
- Working with Files and Directories
- Getting Started to Linux
- Revision of basic Commands
- Access control list and chmod command
- chown and commands
- Network Commands like telnet, ftp, ssh, and sftp,finger
- Use of secondary storage devices (Like: - Hard disk, Floppy, CDROM) in 
-Linux environment and formatting of these devices.
-Lab:
- Getting Acquainted with the Linux Environment
- Use various commands in Linux system.
- (ls, cp, mv, lpr, sort, grep, cat, tac, more, head, tail, man, whatis, 
-whereis, locate, find, diff, file, rm, mkdir, rmdir, cd, pwd, ln and ln –s, gzip 
-and gunzip, zip and unzip,tar and its variants, zcat, cal, bc and bc –l, 
-banner date, time, wc, touch, echo, who, finger, w, whoami, who am i, 
-alias, unalias, touch, push, pop, jobs, ps, etc.)
-Session 3 & 4:
- Installation of Linux
- The interactive Anaconda installer
- A hands-free method of installation
- Understanding the boot procedure
- Configuring the GRUB boot loader
- The Initial RAM Disk
- Understanding run levels
- Repository & Package Management (RPM & DEB)
-ACTS, PUNE
-PG-DITISS 5
-Suggested Teaching Guideline For
-Concept of Operating Systems & Administration PG-DITISS August 2024
-Lab:
- Installation of Linux, configuring Boot Loader, Yellow dog updater 
-configuration
-Session 5:
- Shutdown and Installation concepts
- Kick Start Configuration & Customization
- Deployment using Kickstart
- User administration
-Lab:
- Installation of basic packages, configuring Kickstart, User Administration
-Session 6:
- Network address Ipv4/Ipv6
- Using OpenSSH for network communications
- Using VNC for network communication
- Network Authentication
-Lab:
- Configuring Network address, OpenSSH, VNC
-Session 7:
- User & Group Management
-Lab:
- User & Group Management
-Session 8:
- Disk management
-Lab:
- Fdisk, gdisk, LVM
-Session 9:
- Network Implementation & print services
-Lab:
- Network Implementation & print services
-ACTS, PUNE
-PG-DITISS 6
-Suggested Teaching Guideline For
-Concept of Operating Systems & Administration PG-DITISS August 2024
-Session 10:
- Services Management
- System Configuration Files
-Lab:
- Working with System configuration files, NIS Configuration
-Session 11:
- Patches
- System Management
- X configuration server
-Lab:
- Working with patch management, X configuration
-Session 12:
- Configuration of DNS
-Lab:
- Configuration of DNS
-Session 13:
- Introduction to Configuring Services
- Setting up an NFS server
- Setting up an FTP server
-Lab:
- Configuring NFS
-Session 14:
- The Samba Server: networking with Windows system
- Configuring a DHCP server
- Configuring a DNS server
-Lab:
- Configuring Samba, DHCP Server, DNS Server
-Session 15:
- Configuring the Apache web server
- Apache security & Virtual Hosting
- Configuring the Squid web proxy cache
-ACTS, PUNE
-PG-DITISS 7
-Suggested Teaching Guideline For
-Concept of Operating Systems & Administration PG-DITISS August 2024
-Lab:
- Configuring Apache Web Server, Virtual hosting, Squid Proxy
-Session 16:
- Understanding e-mail delivery
- Postfix Mail Server
- Dovecot: an IMAP and POP server
- squirrel Mail Web Client
-Lab:
- Configuring Postfix, Dovecot, Squirrel Mail
-Session 17:
- Introduction to Performance Tuning
- Maintenance and troubleshooting
- The Threat Model and Protection Methods
-Session 18:
- Basic Service Security
- Logging and NTP
- BIND and DNS Security
-Session 19:
- Network Authentication: LDAP & NIS
- Apache Clustering
- Load Balancer
-Lab:
- Configure and implement of LDAP.
- Configure and implement of NIS.
-Session 20:
- Virtual Machine management
- Virtual machine Network Configuration
-Session 21 & 22:
- Introduction to BASH Command Line Interface (CLI) Error Handling
- Debugging & Redirection of scripts
- Control Structure, Loop
- Variable & String
- Conditional Statement Regular Expressions
-ACTS, PUNE
-PG-DITISS 8
-Suggested Teaching Guideline For
-Concept of Operating Systems & Administration PG-DITISS August 2024
-Lab:
- Working to automate bash commands using bash scripts.
- Bash script using loops, Strings, regular expressions
-Session 23:
- Automate Task Using Bash Script
- Security patches
-Lab:
- Working to automate bash commands using bash scripts.
-Session 24:
- Logging & Monitoring using script
-Lab:
- Working to automate bash commands using bash scripts.
-Session 25:
- Case Study
+Suggested Teaching Guideline for
+Programming Concepts PG-DITISS August 2024
+Lab Assignment:
+ Utilize aggregate functions (SUM, COUNT, AVG) to perform calculations 
+on dataset columns.
+ Employ GROUP BY clauses to group data based on specified criteria.
+ Use HAVING clauses to filter grouped data based on aggregate 
+conditions.
+(Python) 24 Theory + 26 Lab
