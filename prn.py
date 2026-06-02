7d99e749829dc837e05fd4e4365a9ed601ee966d
siddharthpagare01 <143196285+siddharthpagare01@users.noreply.github.com>
2026-06-02 12:38:31 +0530

    Update prn.py

--- prn.py
+++ prn.py
@@ -1,167 +1,269 @@
-ACTS, Pune
-PG-DBDA Page 1 of 5
-Suggested Teaching Guidelines for
-Linux Programming and Cloud Computing
-PG-DBDA August 2025
-Duration: 28 Classroom hours + 22 Lab hours
-Objective: To introduce Linux environment and hands on Linux commands. Prerequisites: Knowledge of Computer Fundamentals
-Evaluation method: Theory exam– 40% weightage
-Lab exam – 40% weightage
-Internal exam– 20% weightage
+ACTS, PUNE
+PG-DITISS 1
+Suggested Teaching Guideline For
+Concept of Operating Systems & Administration PG-DITISS August 2024
+Duration: 90 Classroom hrs + 90 Lab hrs (Total: 180 Hrs)
+Objective: To introduce the student to concepts of Operating Systems & Administration.
+Evaluation method: Theory exam – 40% weightage
+ Lab exam – 40% weightage 
+ Internal Assessment – 20% weightage
 List of Books / Other training material
-Reference:
-1. Linux: The Complete Reference – Petersen/ TMH 6
-th Edition
-2. The Linux Programming Interface: Linux and UNIX System Programming Handbook
-3. Pro Bash Programming: Scripting the GNU/Linux Shell, Second Edition
-4. Beginning Unix – Joe Marilino (Wrox Publication)
-5. Linux Command Line and Shell Scripting Bible – Blum (Wiley – India)
-6. Cloud Computing Black Book by Kailash Jayaswal, Dreamtech
-7. Mastering Cloud Computing by Rajkumar/ McGraw Hill Education
-8. Cloud Computing a Practical Approach by Anthony T. Velte/ McGraw Hill Education
-9. Architecting the Cloud: Design Decisions for Cloud Computing Service Models (SaaS, PaaS, and IaaS)
-10. Cloud Computing
-11. An Introduction to Parallel Computing: Design and Analysis of Algorithms (Authors: Vipin
-Kumar, Ananth Grama, Anshul Gupta, George Karypis)
-12. High Performance Cluster Computing: Architectures & Systems (Volume-1) by Rajkumar
-Buyya, Pearson
-13. Parallel Programming in C with MPI and Open MPI, Michael, TMH
-14. High-Performance Computing on Complex Environments
-Note: Each session having 2 Hours & 2 Hours of Lab unless indicated otherwise.  Linux Programming (12T + 12L)
+Courseware: The Complete Reference Linux by Richard Petersen
+Reference book:
+• Mastering Windows Server 2016 R2 by Brian Svidergol, Vladimir Meloski
+• Windows Server 2022 Administration Fundamentals by Bekim Dauti
+Note: Encourage candidates to implement lab using scripts.
+Windows Operating System and Security Issue (80 Hrs - 40 Hrs Theory + 40 Hrs Lab)
 Session 1 & 2:
-Linux History and Operation
-o The Evolution of Linux
-o The GNU Movement and the GPL
-o Linux Operations as a Server
-o The Architecture and Structure of Linux
-ACTS, Pune
-PG-DBDA Page 2 of 5
-Suggested Teaching Guidelines for
-Linux Programming and Cloud Computing
-PG-DBDA August 2025
-Installing and Configuring Linux (Ubuntu and CentOS)
-o Introduction to Installation and Media Types
-o Performing a Custom Linux Server Installation
-o Run Levels and the Startup/Shutdown Sequence
-o Logging In and Out of a Linux System
-Basic Commands
-(ls, cp, mv, sort, grep, cat, head, tail, man, locate, find, diff, file, rm, mkdir, rmdir, cd, pwd, ln and ln -s, gzip and gunzip, zip and unzip, tar and its variants, touch, echo, who, whoami, ps, kill, makefile, etc.)
-Assignment – Lab:
-1. Getting Acquainted with the Linux Environment
-2. Use various commands in the Linux system. 3. As root, create a directory dbda and under it create a directory named test and
-create 100 files under it with names file1, file2, ..., file100 - all this using a single
-command. Session 3:
-Gaining Confidence with Linux
-o Access control list and chmod command, chown and chgrp commands
-o Commands like telnet, ftp, ssh, and sftp
-o Basics of I/O system with mount and unmount. Vi/vim/gedit Editor
-o Features and different modes of vi editor
-o Editing using vi editor
-o Find and replace commands
-o Cut-copy-paste commands
-o The set command
-o Other related commands of vi
-Assignment – Lab:  Create the file /tmp/acl_file. 1. Allow Larry and Curly to rwx the file. 2. Don’t allow Moe to access the file (rwx). 3. All members of the group stooges (except Moe) should be able to access the
-file (rw).
-ACTS, Pune
-PG-DBDA Page 3 of 5
-Suggested Teaching Guidelines for
-Linux Programming and Cloud Computing
-PG-DBDA August 2025
-Session 4, 5, & 6:
-Linux Shell Programming
-o Introduction to Shells
-a.shell
-b.Different types of Linux shells
-c.Bourne Again Shell (BASH)
-d.Shell variables (environment and user-defined)
-e.Shell files (.bashrc, .profile, .bash_profile, .bash_logout)
-f. Positional parameters
-o Get started with simple scripts (User variable, expr, multiple commands)
-o Wildcards (* and ?)
-o Command line arguments
-o Arithmetic in shell scripts
-o read and echo commands in shell scripts
-o The tput command
-o Taking decisions: • if-then-fi • if-then-else-fi • The test command (file tests, string tests)
-• Nested if-else
-• The case control structure
-o The loop control structure
-• The while, until, and for loop structures
-• The break and continue statements
-o Shell metacharacters
-o Command line expansion
-o Directory stacks manipulation
-o Job control, history, and processes
-o Built-ins and functions
-o Shell Files
-Assignment – Lab:
-• Change the shell of user3 to nologin. Now login as user4 and try to switch to user3. Observe the result. • Login as root, create a file filewithacl and apply ACL on it in such a way that only user5
-is able to read and write to it. Note: root will do all its work under the dbda folder.
-ACTS, Pune
-PG-DBDA Page 4 of 5
-Suggested Teaching Guidelines for
-Linux Programming and Cloud Computing
-PG-DBDA August 2025
- Cloud Computing (16T + 10L)
-Session 7:
-Git / GitHub
-o Introduction to Version Control Systems
-o Creating GitHub repository
-o Using Git – Introduction to Git commands. Session 8: (2T)
-o Introduction to Cloud
-o paradigms
-o Characteristics and benefits
-o Understanding Cloud Vendors (AWS/Azure/GCP)
-o Definition
-o Characteristics
-o Components
-Lab Assignments:
-o Study about cloud and other similar configurations. o Explore available solutions. Session 9 & 10: (4T)
-o Introduction to SaaS
-o Pros and Cons of SaaS Model
-o Traditional Packaged Software vs. SaaS
-o SaaS Examples
-o Introduction to IaaS
-o Examples
-o Introduction to Virtualization
-o Types and Uses of Virtualization
-o Virtual Machine Provisioning
-o Virtual Machine Migration Services
-o Private Cloud Computing Deployment
-o Introduction to PaaS
-o Challenges of Cloud Environment
-o Hypervisor
-o Comparisons of Web Services
-o Organizational Scenarios of Clouds
-ACTS, Pune
-PG-DBDA Page 5 of 5
-Suggested Teaching Guidelines for
-Linux Programming and Cloud Computing
-PG-DBDA August 2025
-Lab Assignments:
-o Provide a solution on cloud as SaaS using available systems. Session 11 & 12:
-o Administering & Monitoring Cloud Services
-o Benefits and limitations
-o Deploy application over cloud. o Comparison among SaaS, PaaS, IaaS
-o Cloud Computing Basics
-o Cloud Products and Solutions
-o Cloud Pricing
-o Compute Products and Services
-Session 13 & 14:
-o Elastic Cloud Compute
-o Dashboard
-o Launching Linux VM
-o Accessing Linux VM
-o Launching & Accessing Windows Server VM
-o Introduction to AWS
-o Introduction to Virtual Private Cloud (VPC) Setup
-o Services provided by AWS: EC2, Lambda, AWS Storage Services S3
-o Introduction to Azure
-• Azure Platform (Azure Portal, Azure CLI, Azure PowerShell)
-• Azure Data Services (Azure SQL Database, Cosmos DB)
-• Azure Storage (Blob Storage, File Storage, Queue Storage)
-• Azure Functions
+ Overview of windows operating system
+ Installation of windows operating system
 Lab:
-o Study about cloud and other similar configurations. o Exposure to big data technologies on cloud. o Create AWS EC2 instance. o Create AWS Lambda. o Create AWS Storage Services S3. o Create AWS VPC. o Deployment of application on AWS using GitHub. o Deployment of application on Azure using GitHub. o Deployment of application on Azure using Azure DevOps. o Deploy AI and analytics workloads on cloud environments with a sample mini
-project.
+ Install, upgrade, and migrate servers and workloads
+ Create, manage, and maintain images for deployment
+Session 3:
+ Concept of Windows Server Backup (WSB)
+Lab:
+ Implement Windows Server Backup (WSB)
+Session 4 & 5:
+ Implementation, planning and maintaining of active directory 
+infrastructure
+Lab:
+ Implementing and administering Active Directory
+ User accounts and groups in an Active Directory Domain
+ Implementing an Active Directory Domain Services (ADDS)
+ Domain Controller (DC)
+ Additional Domain Controller (ADC)
+ Client (Windows 10/11)
+ Member Server (MS)
+ACTS, PUNE
+PG-DITISS 2
+Suggested Teaching Guideline For
+Concept of Operating Systems & Administration PG-DITISS August 2024
+Session 6 & 7:
+ Configuring DNS
+ Implement DHCP and IPAM
+Lab:
+ Configuration of DNS server
+ Install and configure DHCP
+ Manage and maintain DHCP
+ Implement and Maintain IP Address Management (IPAM)
+Session 8:
+ Local policies and Group Policies
+Lab:
+ Implement Local policies & Group policies
+Session 9:
+ Implement Network Connectivity and Remote Access Solutions
+Lab:
+ Implement network connectivity solutions
+ Implement virtual private network (VPN) and Direct Access solutions
+Session 10:
+ Concept of IIS Web Server
+Lab:
+ Implement of IIS Web Server
+Session 11:
+ Concept of Core and Distributed Network Solutions
+Lab:
+ Implement IPv4 and IPv6 addressing
+ Implement Distributed File System (DFS) and Branch Office solutions
+Session 12:
+ Concept of Windows Deployment Service (WDS)
+ACTS, PUNE
+PG-DITISS 3
+Suggested Teaching Guideline For
+Concept of Operating Systems & Administration PG-DITISS August 2024
+Lab:
+ Deploying Windows 10/11 using WDS
+Session 13 & 14: Hyper-V & Storage Solutions
+ Implement Hyper-V
+ Configure disks and volumes
+ Implement server storage
+ Implement data deduplication
+Lab:
+ Install and configure Hyper-V
+ Configure virtual machine (VM) setting
+Session 15:
+ Concept of File Server Resource Manager (FSRM)
+Lab:
+ Implement File Server Resource Manager (FSRM)
+Session 16:
+ Concept of Network Policy Server (NPS)
+Lab:
+ Implement Network Policy Server (NPS)
+Session 17:
+ Concept of Network Load Balancing (NLB)
+ Troubleshooting
+Lab:
+ Implement of Network Load Balancing (NLB)
+ Maintenance and troubleshooting
+ Introduction to Microsoft Windows 10 & 11 security
+ Security issues at the Active Directory level
+ Evaluating and analyzing workstation security
+ Securing Windows services
+Session 18:
+ Concept of Exchange server
+Lab:
+ Installing and implementing of Exchange Server.
+ACTS, PUNE
+PG-DITISS 4
+Suggested Teaching Guideline For
+Concept of Operating Systems & Administration PG-DITISS August 2024
+Session 19 & 20:
+ Power Shell
+Lab:
+ Windows Power Shell Technology Background and Overview
+ Power shell Error Handling and Debugging
+ Windows Administration using power shell
+ Background Jobs and Remote Administration
+ Windows Power Shell Tips & Re-Using Scripts and Functions
+Linux Operating System and Security (100Hrs) (50 Hrs Theory + 50 hrs Lab)
+Session 1 & 2:
+ Introduction to Linux
+ The Linux File System
+ Working with Files and Directories
+ Getting Started to Linux
+ Revision of basic Commands
+ Access control list and chmod command
+ chown and commands
+ Network Commands like telnet, ftp, ssh, and sftp,finger
+ Use of secondary storage devices (Like: - Hard disk, Floppy, CDROM) in 
+Linux environment and formatting of these devices.
+Lab:
+ Getting Acquainted with the Linux Environment
+ Use various commands in Linux system.
+ (ls, cp, mv, lpr, sort, grep, cat, tac, more, head, tail, man, whatis, 
+whereis, locate, find, diff, file, rm, mkdir, rmdir, cd, pwd, ln and ln –s, gzip 
+and gunzip, zip and unzip,tar and its variants, zcat, cal, bc and bc –l, 
+banner date, time, wc, touch, echo, who, finger, w, whoami, who am i, 
+alias, unalias, touch, push, pop, jobs, ps, etc.)
+Session 3 & 4:
+ Installation of Linux
+ The interactive Anaconda installer
+ A hands-free method of installation
+ Understanding the boot procedure
+ Configuring the GRUB boot loader
+ The Initial RAM Disk
+ Understanding run levels
+ Repository & Package Management (RPM & DEB)
+ACTS, PUNE
+PG-DITISS 5
+Suggested Teaching Guideline For
+Concept of Operating Systems & Administration PG-DITISS August 2024
+Lab:
+ Installation of Linux, configuring Boot Loader, Yellow dog updater 
+configuration
+Session 5:
+ Shutdown and Installation concepts
+ Kick Start Configuration & Customization
+ Deployment using Kickstart
+ User administration
+Lab:
+ Installation of basic packages, configuring Kickstart, User Administration
+Session 6:
+ Network address Ipv4/Ipv6
+ Using OpenSSH for network communications
+ Using VNC for network communication
+ Network Authentication
+Lab:
+ Configuring Network address, OpenSSH, VNC
+Session 7:
+ User & Group Management
+Lab:
+ User & Group Management
+Session 8:
+ Disk management
+Lab:
+ Fdisk, gdisk, LVM
+Session 9:
+ Network Implementation & print services
+Lab:
+ Network Implementation & print services
+ACTS, PUNE
+PG-DITISS 6
+Suggested Teaching Guideline For
+Concept of Operating Systems & Administration PG-DITISS August 2024
+Session 10:
+ Services Management
+ System Configuration Files
+Lab:
+ Working with System configuration files, NIS Configuration
+Session 11:
+ Patches
+ System Management
+ X configuration server
+Lab:
+ Working with patch management, X configuration
+Session 12:
+ Configuration of DNS
+Lab:
+ Configuration of DNS
+Session 13:
+ Introduction to Configuring Services
+ Setting up an NFS server
+ Setting up an FTP server
+Lab:
+ Configuring NFS
+Session 14:
+ The Samba Server: networking with Windows system
+ Configuring a DHCP server
+ Configuring a DNS server
+Lab:
+ Configuring Samba, DHCP Server, DNS Server
+Session 15:
+ Configuring the Apache web server
+ Apache security & Virtual Hosting
+ Configuring the Squid web proxy cache
+ACTS, PUNE
+PG-DITISS 7
+Suggested Teaching Guideline For
+Concept of Operating Systems & Administration PG-DITISS August 2024
+Lab:
+ Configuring Apache Web Server, Virtual hosting, Squid Proxy
+Session 16:
+ Understanding e-mail delivery
+ Postfix Mail Server
+ Dovecot: an IMAP and POP server
+ squirrel Mail Web Client
+Lab:
+ Configuring Postfix, Dovecot, Squirrel Mail
+Session 17:
+ Introduction to Performance Tuning
+ Maintenance and troubleshooting
+ The Threat Model and Protection Methods
+Session 18:
+ Basic Service Security
+ Logging and NTP
+ BIND and DNS Security
+Session 19:
+ Network Authentication: LDAP & NIS
+ Apache Clustering
+ Load Balancer
+Lab:
+ Configure and implement of LDAP.
+ Configure and implement of NIS.
+Session 20:
+ Virtual Machine management
+ Virtual machine Network Configuration
+Session 21 & 22:
+ Introduction to BASH Command Line Interface (CLI) Error Handling
+ Debugging & Redirection of scripts
+ Control Structure, Loop
+ Variable & String
+ Conditional Statement Regular Expressions
+ACTS, PUNE
+PG-DITISS 8
+Suggested Teaching Guideline For
+Concept of Operating Systems & Administration PG-DITISS August 2024
+Lab:
+ Working to automate bash commands using bash scripts.
+ Bash script using loops, Strings, regular expressions
+Session 23:
+ Automate Task Using Bash Script
+ Security patches
+Lab:
+ Working to automate bash commands using bash scripts.
+Session 24:
+ Logging & Monitoring using script
+Lab:
+ Working to automate bash commands using bash scripts.
+Session 25:
+ Case Study
