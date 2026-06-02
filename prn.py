e959c921265bb9503aaf8586dd4654f5c54336d4
siddharthpagare01 <143196285+siddharthpagare01@users.noreply.github.com>
2026-06-02 11:05:00 +0530

    Update prn.py

--- prn.py
+++ prn.py
@@ -1,4 +1,189 @@
-
+PG-DITISS 1 PG-DITISS 1 PG-DITISS 1
+ACTS, PUNE Suggested Teaching Guideline for
+Fundamentals of Computer NetworksPG-DITISS August 2024
+Duration: 80 classroom hrs (40hrs theory + 40 lab hrs)
+Objective: To introduce the student to fundamentals of computers and networks
+Evaluation method: Theory exam – 40% weightage
+Lab exam – 40% weightage
+Internal Assessment– 20% weightage
+List of Books / Other training material
+Courseware: Data Communications and Networking, Behrouz A. Forouzan, 
+McGrawHill Education; Fifth edition
+Session 1 & 2:
+ Internetworking
+ OSI model
+ Ethernet
+ Wireless Networking
+Assignment: 
+1) Difference between UTP & STP
+2) Write categories of cables
+3) e in CAT5e
+4) OSI Model
+Session 3:
+ Internet Protocol
+ TCP/IP model
+Assignment:
+1) IP
+2) TCP/IP Model
+3) Write a difference between TCP & UDP.
+Session 4 & 5:
+ IP Subnetting & variable Length Subnet Masking
+Assignment:
+1) Subnetting
+2) Determine the network and host part of 192.168.5.85 /24, 
+10.128.240.50/30 address.
+3) Divide the network as per following requirement 192.168.1.0/24
+a. Subnet 1 =28 hosts
+b. Subnet 2 =52 hosts
+c. Subnet 3 =15 hosts
+d. Subnet4 = 5 hosts
+PG-DITISS 2 PG-DITISS 2 PG-DITISS 2
+ACTS, PUNE Suggested Teaching Guideline for
+Fundamentals of Computer NetworksPG-DITISS August 2024
+Session 6:
+ Router IOS & Security Device Manager
+Assignment:
+1) Subnet the Class C IP Address 195.1.1.0 So that you have 10 subnets 
+each with a maximum 12 hosts on each subnet. List the Address on 
+host 1 on subnet 0,1,2,3,10.
+2) Divide the network to find 500 hosts in each subnet.
+152.152.0.0 /16
+Session 7:
+ Managing an Internetworking Router
+Lab Assignments:
+Working with Router Booting, configuration registers, Router IOS, Telnet, 
+ResolvingHostname, Debugging.
+Session 8:
+ Static Routing
+ Dynamic Routing
+ Routing Protocols
+Theory Assignment:
+ Dynamic Routing
+ Static Routing
+ Routing Protocols
+Session 9 & 10:
+ Implementing of Routing Protocols
+Lab Assignments:
+ Implementation of Static Routing, RIP, IGRP, EIGRP, OSPF
+Session 11:
+ Layer 2 switching
+Theory Assignment:
+1) Spanning Tree Protocol (STP)?
+2) Write types of Spanning Tree Protocol (STP).
+3) priority number of Spanning Tree Protocol (STP)
+4) disable Spanning Tree Protocol (STP)
+Lab Assignments:
+1. Configuration of switch, STP
+2. Create the following topology for STP and configure a specific switch as root 
+bridge by changing priority number.
+PG-DITISS 3 PG-DITISS 3 PG-DITISS 3
+ACTS, PUNE Suggested Teaching Guideline for
+Fundamentals of Computer NetworksPG-DITISS August 2024
+Session 12:
+ Virtual LANs
+Lab Assignments:
+1. Configure, verify, and troubleshoot VLANs (normal/extended range) 
+spanning multipleswitches, inter-VLAN routing
+2. Create following topology where Switch 1 is Server mode and switch 2,3,4,5 
+are client mode. Use network 192.168.10.0 /24
+ Transfer the Vlan info from server to all clients.
+ Assign vlan as mention in the topology.
+ PC’s in having same vlan should ping each other. 
+3. Create following topology where Switch 1 is Server mode and switch 2,4, are 
+client mode.
+Use network 192.168.1.0 /24
+ Transfer the Vlan info from server to all clients.
+ Assign vlan as mention in the topology.
+ PC’s in having same vlan should ping each other. 
+ Configured switch 3 in transfer mode and transfer vlan info to switch 4
+Session 13:
+ Infrastructure Security
+Lab Assignments:
+ Configure, verify, and troubleshoot port security, describe common 
+access layer threat mitigation, Configure, verify, and troubleshoot IPv4 
+and IPv6 access list fortraffic filtering techniques,
+ Verify ACLs using the APIC-EM Path Trace ACL analysis tool
+ Configure, verify, and troubleshoot basic device hardening
+ Describe device security using AAA with TACACS+ and RADIUS
+Session 14:
+ NAT
+ IPV6
+ WAN Technologies
+Lab Assignments
+NAT configuration, Configuring Routers with IPv6
+Session: 15
+Lab Assignments:
+ Configure and verify PPP and MLPPP on WAN interfaces using local
+authentication
+ Configure, verify, and troubleshoot PPPoE client-side interfaces using
+localauthentication, Configure, verify, and troubleshoot GRE tunnel
+connectivity.
+ Configure and verify single-homed branch connectivity using eBGP IPv4
+(limitedto peering and route advertisement using Network command only)
+PG-DITISS 4 PG-DITISS 4 PG-DITISS 4
+ACTS, PUNE Suggested Teaching Guideline for
+Fundamentals of Computer NetworksPG-DITISS August 2024
+Session 16:
+ Introduction to SDN,
+ Overview and Architecture of SDN
+ Scalability (Data Centers, Service provider networks, ISP Automation)
+ Reliability (QoS, and Service Availability)
+ Consistency (Configuration management, and Access Control Violations)
+ Opportunities and Challenges
+Theory Assignment:
+ SDN and Architecture of SDN.
+Session 17:
+ Virtual networking
+ Use-cases (Network Access Control, Virtual Customer Edge, 
+DatacenterOptimization)
+Theory Assignment:
+ Virtual networking
+ Use cases of Network Access Control, Virtual Customer Edge, Datacenter
+Optimization
+Session 18:
+ Introduction to OpenFlow
+ History and evolution
+ Control and data plane separation
+ OpenDaylight architecture overview
+Lab Assignment:
+ Understanding the classroom environment
+ Installing OpenDaylight
+ Initial Configuration
+ Initial Controller Interations
+Session 19:
+Getting Started with Opendaylight
+ How to get started
+ OpenDaylight clustering
+ Model-Driven Service Abstraction Layer
+ Internal datastore
+ OpenFlow plugin
+ OpenVSwitch concepts
+ Mininet overview
+ L2Switch Application
+PG-DITISS 5 PG-DITISS 5 PG-DITISS 5
+ACTS, PUNE Suggested Teaching Guideline for
+Fundamentals of Computer NetworksPG-DITISS August 2024
+Lab Assignment:
+ Configuring an OpenDaylight Cluster
+ Initial Command Line Interactions
+ Enabling Required Features
+ Launching the User Interface
+ Creating an Emulated SDN Network
+ Running L2Switch
+Session 20:
+Getting more from Opendaylight
+ OpenDaylight and AAA
+ Introduction to OVSDB Virtualization
+ Application Intents and Group Based Policy
+ Service Function Chaining
+ LISP Flow Mapping
+ Virtual Tenant Networks
+Lab Assignment:
+ Advanced AAA integrations
+ Creating Virtual Networks
+ Working with policies and intents
+ Initial Service Function Chaining
+ OpenDaylight and OpenVSwitch
 
 print ("240840127035")
 
